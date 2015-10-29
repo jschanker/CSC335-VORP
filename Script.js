5e91701922ce7ae0a911b000fdebd7ac78f13cd6
Yago <santiago_x06@hotmail.com>
2015-10-29 19:29:52 -0400

    Segregate Name
    Initial work done to segregate the data based on the number of columns.
    We should be smarter and use jQuery to select the 2nd, 3rd, 4th columns
    for us.

--- Script.js
+++ Script.js
@@ -7,16 +7,22 @@ function jsdomjQueryConnector(htmlURL, callback) {
 
 
 // Run this code
+var columns = 19;
 var url = "http://www.baseballprospectus.com/sortable/index.php?cid=1819072";
 jsdomjQueryConnector(url, function (err, window, callback) {
   var errMessage = "Error encountered when trying to get rows.";
-  var rowJQuerySelector = ".TTdata, .TTdata_ltgrey";
+  var rowJQuerySelector = ".TTdata td, .TTdata_ltgrey td";
   if (err) {
    console.log("Err: ", errMessage, " ... exiting");
    return;
   }
-  window.$(rowJQuerySelector).each(function() {
-    //Here you should process the data and write to a file or database
-    console.log(this);
+  var $ = window.$;
+  var counter = 0;
+  $(rowJQuerySelector).each(function() {
+    if (counter % columns === 1) {
+        //Here you should process the data and write to a file or database
+        console.log($(this).text())
+    }
+    counter++;
   });
 });
