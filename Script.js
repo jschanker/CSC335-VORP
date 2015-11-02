253f5dbe731a9dfd2a0ccc4a4d607760df92a160
Santiago Vargas <santiago_x06@hotmail.com>
2015-11-02 16:28:43 -0500

    Created Player Objects
    The code stores player positions and vorp by player name in the
    playersObject

--- Script.js
+++ Script.js
@@ -5,24 +5,44 @@ function jsdomjQueryConnector(htmlURL, callback) {
   jsdom.env(htmlURL, jsdomScripts, callback);
 }
 
+//The code below will be run
 
-// Run this code
 var columns = 19;
+// This variable should be automatically built
+var rowIndex = {
+  name: 1,
+  position: 3,
+  vorp: 16
+}
 var url = "http://www.baseballprospectus.com/sortable/index.php?cid=1819072";
 jsdomjQueryConnector(url, function (err, window, callback) {
   var errMessage = "Error encountered when trying to get rows.";
-  var rowJQuerySelector = ".TTdata td, .TTdata_ltgrey td";
+  var rowJQuerySelector = "tr.TTdata, tr.TTdata_ltgrey";
   if (err) {
    console.log("Err: ", errMessage, " ... exiting");
    return;
   }
-  var $ = window.$;
-  var counter = 0;
-  $(rowJQuerySelector).each(function() {
-    if (counter % columns === 1) {
-        //Here you should process the data and write to a file or database
-        console.log($(this).text())
-    }
-    counter++;
+  //Set jQuery variable alias
+  var jQuery = window.$;
+  //Save all rows to an array using jQuery
+  var rows = jQuery(rowJQuerySelector).toArray();
+  //Create an object of player objects
+  var playersObject = {};
+  //Loop through each row and create player object
+  rows.forEach(function(current){
+    //Attach a function to each row to easily retireve cell values
+    current.getCellText = function(index) {
+      //Returns the text at the cell specified by the index
+      return jQuery(current).find("td:eq(" + index + ")").text();
+    };
+    //Get the player's name which is the 2nd cell
+    var name = current.getCellText(rowIndex.name);
+    //Create the new player object
+    playersObject[name] = {
+      position: current.getCellText(rowIndex.position),
+      vorp: current.getCellText(rowIndex.vorp)
+    };
   });
+  //Log all players
+  console.log(playersObject);
 });
