5d64af253863f98ed139fb4debd8f29a8cf0eb12
Yago <santiago_x06@hotmail.com>
2015-11-04 01:00:39 -0500

    Add Player URL to Player Object
    The code now obtains the personal URL to the individual web page of each
    player and stores it in the player object.

--- Script.js
+++ Script.js
@@ -27,7 +27,7 @@ var rowIndex = {
 }
 var url = "http://www.baseballprospectus.com/sortable/index.php?cid=1819072";
 jsdomjQueryConnector(url, function (err, window, $) {
-  var errMessage = "Error encountered when trying to get rows.";
+  var errMessage = "Error encountered when trying to get page html.";
   //Select all table rows with the "TTdata" or "TTdata_ltgrey" classes
   var rowJQuerySelector = "tr.TTdata, tr.TTdata_ltgrey";
   if (err) {
@@ -41,16 +41,21 @@ jsdomjQueryConnector(url, function (err, window, $) {
   //Loop through each row and create player object
   rows.forEach(function(current){
     //Attach a function to each row to easily retireve cell text
-    current.getCellText = function(index) {
+    current.getCell = function(index) {
       //Returns the text at the cell specified by the index
-      return $(current).find("td:eq(" + index + ")").text();
+      return $(current).find("td:eq(" + index + ")");
     };
+    //Get the player's name cell
+    var nameCell = current.getCell(rowIndex.name);
+    //Get the player's personal url
+    var playerURL = nameCell.find("a").prop("href");
     //Get the player's name
-    var name = current.getCellText(rowIndex.name);
+    var name = nameCell.text();
     //Create the new player object
     playersObject[name] = {
-      position: current.getCellText(rowIndex.position),
-      vorp: current.getCellText(rowIndex.vorp)
+      url: playerURL,
+      position: current.getCell(rowIndex.position).text(),
+      vorp: current.getCell(rowIndex.vorp).text()
     };
   });
   //Log all players
