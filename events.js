14fc6ef9def681fd40e633eedb7bc78b8708317b
HeathFeath49 <hstarkie@lions.molloy.edu>
2015-11-05 14:18:08 -0500

    Code Cleanup
    -changed names of variables holding arrays
    -changed name of variables holding converted salary
    -separated main sections of code with comments

--- events.js
+++ events.js
@@ -1,25 +1,44 @@
+
+//Event handler for button
 var computeButton = document.getElementById("computeButton");
 computeButton.addEventListener("click", function() {
 	    var rawData = (document.getElementById("player-data").value);
 	    alert(rawData);
-	      });
-eachLineIsValueInArray=rawData.split("\n");//Each player & their info==1 value in array
+});
+
+//Seperate lines into own array
+lineArr=rawData.split("\n");//Each player & their info==1 value in array
 players={};
 playerName=null;
-for(var i=0;i<eachLineIsValueInArray.length;i++){
-    var personsInfoIsValueInArray=eachLineIsValueInArray[i].split("\t");
-    playerName=personsInfoIsValueInArray[0];
+
+//Seperate info for each player into arry
+for(var i=0;i<lineArr.length;i++){
+    var playerInfoArr=lineArr[i].split("\t");
+    
+    
+    //create object with key of player name
+    playerName=playerInfoArr[0];
     players[playerName]={};
-    //Array within array. In each inner array, each piece of info is one value.
-    //personsInfoIsValueInArray.forEach(function(value,index){
-    players[playerName].position=personsInfoIsValueInArray[2];
-    players[playerName].vorp=Number(personsInfoIsValueInArray[13]);
-        if(personsInfoIsValueInArray[3].includes("$")){
+    
+    //set position attribute in player's object
+    players[playerName].position = playerInfoArr[2];
+    
+    //set vorp attribute in player's object
+    players[playerName].vorp=Number(playerInfoArr[13]);
+        
+        //Do we need this if statement? 
+        if(playerInfoArr[3].includes("$")){
             //Wow. such money. much tedious. very spook. wow
-            var removeDollarSign=personsInfoIsValueInArray[3].split(",").join('');
-            var removeCommas=removeDollarSign.split("$").join('');
-            var convertStringToNumber=Number(removeCommas);
-            players[playerName].salary=convertStringToNumber;
+            //convert salary from string to number
+            var removeSign = playerInfoArr[3].split(",").join('');
+            var removeCommas=removeSign.split("$").join('');
+            var salAsNum=Number(removeCommas);
+            players[playerName].salary=salAsNum;
             //Thnk u ( ͡° ͜ʖ ͡ °)
         }
     }
+    
+//Code Dump
+//Array within array. In each inner array, each piece of info is one value.
+    //playerInfoArr.forEach(function(value,index){
+    
