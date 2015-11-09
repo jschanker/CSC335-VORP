cd759363f1996e21b11da2d47a2bf4be45a3f609
Yago <santiago_x06@hotmail.com>
2015-11-08 22:17:40 -0500

    Salary Obtained from Newsday
    Player salaries are obtained using the Newsday API.

--- Script.js
+++ Script.js
@@ -1,20 +1,29 @@
+/**
+ * Dependencies
+ */
 var cheerio = require("cheerio");
 var async = require("async");
 var request = require("request");
 
-//Global variable denoting the maximum concurrent connections to a website
-var maximumConnections = 50;
-//Base URL
-var baseURL = "http://www.baseballprospectus.com";
+/**
+ * Constants
+ */
 //Replace Constant
 var replaceConstant = "#replaceMe";
-//Salary URL
-var salaryURL = "http://sports.newsday.com/long-island/data/baseball/mlb-salaries-2015/#s:player=" + replaceConstant + "|o:c=salary;d=true;|";
+//Player List URL
+var url = "http://www.baseballprospectus.com/sortable/index.php?cid=1819072";
+//URL to post to when getting the player salary
+var salaryURL = "http://sports.newsday.com/_common/php/templates/modular/v1-1/table";
+//The post body when getting the player salary
+var salaryPostBody = "options%5Btarget%5D=%23sdbTableWrapper&options%5Btype%5D=table&options%5Bsource%5D%5Btype%5D=MySQL&options%5Bsource%5D%5Bid%5D=05f93ab6c67a&options%5Bsort%5D%5Bcolumn%5D=salary&options%5Bsort%5D%5Bdescending%5D=true&options%5Bpagination%5D%5BperPage%5D=50&options%5Bpagination%5D%5Btarget%5D=main+.count&options%5Bpagination%5D%5BcountType%5D=page&options%5Btemplate%5D%5BheaderRow%5D=%3Cth+data-column%3D%22player%22%3E%3Ca+href%3D%22%22%3E%7Bplayer%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22team%22%3E%3Ca+href%3D%22%22%3E%7Bteam%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22position%22%3E%3Ca+href%3D%22%22%3E%7Bposition%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22salary%22%3E%3Ca+href%3D%22%22%3E%7Bsalary%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22age%22%3E%3Ca+href%3D%22%22%3E%7Bage%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22division%22%3E%3Ca+href%3D%22%22%3E%7Bdivision%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22league%22%3E%3Ca+href%3D%22%22%3E%7Bleague%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22state%22%3E%3Ca+href%3D%22%22%3E%7Bstate%7D%3C%2Fa%3E%3C%2Fth%3E&options%5Btemplate%5D%5Brow%5D=%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bplayer%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bteam%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bposition%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%24%7Bsalary%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bage%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bdivision%7D%3C%2Fa%3E%3C%2Ftd%3E%0D%0A%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bleague%7D%3C%2Fa%3E%3C%2Ftd%3E%0D%0A%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bstate%7D%3C%2Fa%3E%3C%2Ftd%3E&options%5Bcolumns%5D%5Bplayer%5D%5Bdisplay%5D=Player&options%5Bcolumns%5D%5Bteam%5D%5Bdisplay%5D=Team&options%5Bcolumns%5D%5Bposition%5D%5Bdisplay%5D=Position&options%5Bcolumns%5D%5Bsalary%5D%5Bdisplay%5D=Salary&options%5Bcolumns%5D%5Bsalary%5D%5Bnumeric%5D=true&options%5Bcolumns%5D%5Bage%5D%5Bdisplay%5D=Age&options%5Bcolumns%5D%5Bage%5D%5Bnumeric%5D=true&options%5Bcolumns%5D%5Bdivision%5D%5Bdisplay%5D=Division&options%5Bcolumns%5D%5Bleague%5D%5Bdisplay%5D=League&options%5Bcolumns%5D%5Bstate%5D%5Bdisplay%5D=State&options%5Bform%5D=form%5Bname%3D%22sdbForm%22%5D&options%5BtopList%5D=%23sdbTopFive&options%5Bfilters%5D=false&options%5BmobileStyle%5D=fixed+left+column&options%5Boffset%5D=0&template%5BheaderRow%5D=%3Cth+data-column%3D%22player%22%3E%3Ca+href%3D%22%22%3E%7Bplayer%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22team%22%3E%3Ca+href%3D%22%22%3E%7Bteam%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22position%22%3E%3Ca+href%3D%22%22%3E%7Bposition%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22salary%22%3E%3Ca+href%3D%22%22%3E%7Bsalary%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22age%22%3E%3Ca+href%3D%22%22%3E%7Bage%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22division%22%3E%3Ca+href%3D%22%22%3E%7Bdivision%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22league%22%3E%3Ca+href%3D%22%22%3E%7Bleague%7D%3C%2Fa%3E%3C%2Fth%3E%0D%0A%3Cth+data-column%3D%22state%22%3E%3Ca+href%3D%22%22%3E%7Bstate%7D%3C%2Fa%3E%3C%2Fth%3E&template%5Brow%5D=%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bplayer%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bteam%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bposition%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%24%7Bsalary%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bage%7D%3C%2Fa%3E%3C%2Ftd%3E%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bdivision%7D%3C%2Fa%3E%3C%2Ftd%3E%0D%0A%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bleague%7D%3C%2Fa%3E%3C%2Ftd%3E%0D%0A%3Ctd%3E%3Ca+href%3D%22%22+class%3D%22filter%22%3E%7Bstate%7D%3C%2Fa%3E%3C%2Ftd%3E&sort%5Bcolumn%5D=salary&sort%5Bdescending%5D=true&pagination%5Btarget%5D=main+.count&search%5Bplayer%5D=" + replaceConstant + "&searchTypes%5Bplayer%5D=partial";
 
+/**
+ * Functions
+ */
 //Attaches and returns a reference jQuery as well as html
 function jQueryConnector(url, callback) {
   //console.log(url)
-  request(baseURL + url, function (err, response, html) {
+  request(url, function (err, response, html) {
     //Callback only with the error if there is an error
     if (err) {
       return callback(err);
@@ -36,13 +45,12 @@ function parseTableRowsToPlayersObject(rowArray, jQuery, callback) {
     };
     //Get the player's name cell
     var nameCell = currentRow.getCell(rowIndex.name);
-    //Get the player's personal url
-    var playerURL = nameCell.find("a").attr("href");
     //Get the player's name
     var name = nameCell.text();
+    //Get encoded name
+    var encodedName = encodeURIComponent(name);
     //Create the new player object
     playersObject[name] = {
-      url: playerURL,
       position: currentRow.getCell(rowIndex.position).text(),
       vorp: currentRow.getCell(rowIndex.vorp).text()
     };
@@ -50,31 +58,39 @@ function parseTableRowsToPlayersObject(rowArray, jQuery, callback) {
   return callback(null, playersObject);
 }
 
-function getPlayerSalary(playerURL, callback) {
-  jQueryConnector(playerURL, function (err, $) {
-    //If there is an error, call the callback with the error
+function getPlayerSalary(playerName, callback) {
+  //Must encode the name before using it. Also spaces in the name should be encoded with a plus sign
+  var encodedName = encodeURIComponent(playerName).replace("%20", "+");
+  //Plug in the name to the post body
+  var postBody = salaryPostBody.replace(replaceConstant, encodedName);
+  //Post to the external salary API
+  request.post(salaryURL, {form: postBody}, function (err, response, body) {
     if (err) {
       return callback(err);
     }
-    var salary = $("#cotsyear_totals").find("td").eq(2).text();
-    //console.log("Salary: " + salary);
+    //The body comes back as a string of JSON. Must parse it into an object
+    var data = JSON.parse(body);
+    //Get the salary of the first player
+    var salary = data.rows[0] ? data.rows[0].salary : undefined;
+    if (data.rows.length > 1) {
+      console.log("More than one player found");
+    }
     return callback(null, salary);
-  })
+  });
 }
 
 
-//
-// The code below will be run...
-//
+/**
+ * Function Calls
+ */
 
 
-// This variable should be automatically built
+// This variable should be built automatically
 var rowIndex = {
   name: 1,
   position: 3,
   vorp: 16
 };
-var url = "/sortable/index.php?cid=1819072";
 jQueryConnector(url, function (err, $) {
   var errMessage = "Error encountered when trying to get page html.";
   if (err) {
@@ -86,18 +102,19 @@ jQueryConnector(url, function (err, $) {
   //Save all rows to an array using jQuery
   var rows = $(rowJQuerySelector).toArray();
   parseTableRowsToPlayersObject(rows, $, function (err, players) {
-    //Log all players
-    //console.log(players);
     //For each player save salary
-    async.eachLimit(players, maximumConnections, function (player, done) {
-      getPlayerSalary(player.url, function (err, salary) {
+    async.forEachOf(players, function (player, key, done) {
+      getPlayerSalary(key, function (err, salary) {
         if (err) {
-          console.log("Error getting player " + player.name + " salary using " + player.url);
+          console.log("Error getting player " + player.name + " salary");
           console.log("Error: " + err);
         }
+        //Set the player salary
         player.salary = salary;
         done();
       });
+    }, function (err) {
+      //console.log(players);
     });
   });
 });
