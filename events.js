e8d765b4be2ffb6f63f94c31044ebc7eefa236c0
zach111694 <vnrueiwoqpwoeiruvn>
2015-11-30 23:06:11 -0500

    ~Simplified regex in split function dividing distinct data based on Dr. Schanker's recommendation on parentheses. ~Moved player_object creation inside else statement so that only player objects with salaries are created, thereby skipping a filtering function that would have been needed. ~Added JSON test alert

--- events.js
+++ events.js
@@ -5,12 +5,13 @@ var computeButton = document.getElementById("computeButton");
 
 computeButton.addEventListener("click", function() {
     var data = (document.getElementById("player-data").value);
-    var dataPool = data.split(new RegExp(/(=+)/));
+    var dataPool = data.split(/=+/);
     var players = dataPool[0].split('\n');
 
-    var salaries = dataPool[2];
+    var salaries = dataPool[1];
 
     var player_object = {};
+    var player_count = 0;
     var test_alert_string = "";
 
     for(var i = 0; i < players.length;i++){
@@ -23,27 +24,31 @@ computeButton.addEventListener("click", function() {
         else {
             var indexOf$ = salaries.indexOf("$", salary_playerIndex);
             specific_salary = salaries.substring(indexOf$ + 1, salaries.indexOf('\t', indexOf$));
-            specific_salary = "$" + specific_salary
+            specific_salary = "$" + specific_salary;
+
+            player_object[specific_player[1]] = {
+                vorp: specific_player[16],
+                position: specific_player[3],
+                salary: specific_salary
+            };
         }
 
 
-        player_object[specific_player[1]] = {
-            vorp: specific_player[16],
-            position: specific_player[3],
-            salary: specific_salary
-        };
-
-        test_alert_string += (
-            specific_player[1] + ':    ' +
-            "VORP = " + specific_player[16] + '    ' +
-            "Position = " + specific_player[3]+ '    ' +
-            "Salary = " + specific_salary + '\n'
-        );
+        if(specific_salary[0] === "$"){
+            player_count += 1;
+            test_alert_string += (
+                specific_player[1] + ':    ' +
+                "VORP = " + specific_player[16] + '    ' +
+                "Position = " + specific_player[3]+ '    ' +
+                "Salary = " + specific_salary + '\n'
+            );
+        }
     }
 
-    console.log(test_alert_string);
-    alert(test_alert_string);
-
+    /* Test alert function with JSON object inside param */
+    // alert(JSON.stringify(player_object));
+    /* Test alert function with string inside param */
+    alert(test_alert_string + "\n" + player_count + " players total");
 
 });
 
