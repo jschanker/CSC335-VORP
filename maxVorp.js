2fd1ef7fd1889d350e50775173324eb0b2812fa7
HeathFeath49 <hstarkie@lions.molloy.edu>
2015-12-16 15:41:03 -0500

    Update maxVorp.js

--- maxVorp.js
+++ maxVorp.js
@@ -1,3 +1,6 @@
+
+    
+
 var maxVorp = function(positionObj,budget,vorpInArr){
     
     //base case
@@ -11,27 +14,60 @@ var maxVorp = function(positionObj,budget,vorpInArr){
         for(var position in positionObj){
             
             for(var player in positionObj[position]){
-                console.log(player[vorp]);
+                //console.log(positionObj[position][player].salary);
                 
-                if(player.salary < budget && player.vorp > 0){
+                if(positionObj[position][player].salary < budget && positionObj[position][player].vorp > 0){
                     
+                    
+                    var teamArr = [player];
+                    var newBudget = budget-positionObj[position][player].salary
+                    vorpInArr += positionObj[position][player].vorp;
                     delete positionObj[position];
-                    vorpInArr += player.vorp;
-                    var teamArr = [player].concat(maxVorp(positionObj,budget-player.salary,vorpInArr));
                     
+                    teamArr = teamArr.concat(maxVorp(positionObj,newBudget,vorpInArr));
+                    console.log(teamArr);
                     //compare
                     if(vorpInArr > maxVorpSoFar){
                         maxVorpSoFar = vorpInArr;
                         maxVorpTeamArr = teamArr;
                     }
-                }
                     
+                }
+                   
                     
             }
                 
         }
+        
         return vorpInArr;
         
     }
     
 };
+
+var positionObj = {
+    "pitchers": {
+        "Matt": {
+            
+            "salary":500,
+            "vorp":30
+        },
+        "Ben": {
+            "salary":400,
+            "vorp":25
+        }
+    },
+    "catchers":{
+        "Carly":{
+            "salary":600,
+            "vorp":40
+        },
+        "Bill":{
+            "salary":200,
+            "vorp":-1
+        }
+    }
+};
+
+console.log(maxVorp(positionObj,1000,0));
+
