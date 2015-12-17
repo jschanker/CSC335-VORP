51c9471aae08c6858b0bf2aff076efb0d8004fa0
Yago <santiago_x06@hotmail.com>
2015-12-16 19:57:21 -0500

    Removed a nested for loop
    Algorithm removed a nested for loop

--- algorithm.js
+++ algorithm.js
@@ -5,21 +5,24 @@ var Process = module.exports = {};
 
 Process.maxVORP = function (positions, budget) {
   var vorp = 0;
-  for (var currentPosition in positions) {
-    var currentPlayerList = positions[currentPosition];
+  var firstPosition = Object.keys(positions)[0];
+  if (firstPosition) {
+    var subListOfPositions = _.clone(positions);
+    delete subListOfPositions[firstPosition];
+    vorp = Process.maxVORP(subListOfPositions, budget);
+    var currentPlayerList = positions[firstPosition];
     for (var playerName in currentPlayerList) {
       var currentPlayer = currentPlayerList[playerName];
       var currentSalary = parseInt(currentPlayer.salary);
       if (currentSalary <= budget) {
         var newBudget = budget - currentPlayer.salary;
-        var subListOfPositions = _.clone(positions);
-        delete subListOfPositions[currentPosition];
+        subListOfPositions = _.clone(positions);
+        delete subListOfPositions[firstPosition];
 
         var newVorp = parseFloat(currentPlayer.vorp) + Process.maxVORP(subListOfPositions, newBudget);
         vorp = Math.max(vorp, newVorp);
       }
     }
-    delete positions[currentPosition];
   }
   //In the case of an empty list or expensive players
   return vorp;
