0ac0a4ce48976823b50da487d783ee91f6d80dd0
Yago <santiago_x06@hotmail.com>
2015-12-16 20:32:34 -0500

    Added memoization
    Added memoization

--- algorithm.js
+++ algorithm.js
@@ -4,26 +4,56 @@ var _ = require("underscore");
 var Process = module.exports = {};
 
 Process.maxVORP = function (positions, budget) {
-  var vorp = 0;
-  var firstPosition = Object.keys(positions)[0];
-  if (firstPosition) {
-    var subListOfPositions = _.clone(positions);
-    delete subListOfPositions[firstPosition];
-    vorp = Process.maxVORP(subListOfPositions, budget);
-    var currentPlayerList = positions[firstPosition];
-    for (var playerName in currentPlayerList) {
-      var currentPlayer = currentPlayerList[playerName];
-      var currentSalary = parseInt(currentPlayer.salary);
-      if (currentSalary <= budget) {
-        var newBudget = budget - currentPlayer.salary;
-        subListOfPositions = _.clone(positions);
-        delete subListOfPositions[firstPosition];
+  //Memo object
+  var memo = {};
 
-        var newVorp = parseFloat(currentPlayer.vorp) + Process.maxVORP(subListOfPositions, newBudget);
-        vorp = Math.max(vorp, newVorp);
+  var mV = function (positions, budget) {
+    //In the case of an empty list the default vorp is 0
+    var vorp = 0;
+    var positionKeys = Object.keys(positions);
+    var firstPosition = positionKeys[0];
+    if (firstPosition) {
+      var subListOfPositions = _.clone(positions);
+      delete subListOfPositions[firstPosition];
+      vorp = mV(subListOfPositions, budget);
+      var currentPlayerList = positions[firstPosition];
+      for (var playerName in currentPlayerList) {
+        var currentPlayer = currentPlayerList[playerName];
+        var currentSalary = parseInt(currentPlayer.salary);
+        if (currentSalary <= budget) {
+          var newBudget = budget - currentPlayer.salary;
+          subListOfPositions = _.clone(positions);
+          delete subListOfPositions[firstPosition];
+
+          var lengthOfPositions = positionKeys.length;
+          var newVorp;
+          if (!isInMemo(newBudget, lengthOfPositions)) {
+            newVorp = parseFloat(currentPlayer.vorp) + mV(subListOfPositions, newBudget);
+            addToMemo(budget, lengthOfPositions, newVorp);
+          } else {
+            newVorp = getMemo(budget, lengthOfPositions);
+          }
+          vorp = Math.max(vorp, newVorp);
+        }
       }
     }
+    return vorp;
+  };
+
+  function isInMemo(budget, positionsLeft) {
+    return memo && memo[positionsLeft] && memo[positionsLeft][budget] !== undefined;
   }
-  //In the case of an empty list or expensive players
-  return vorp;
+
+  function getMemo(budget, positionsLeft) {
+    return memo[positionsLeft][budget];
+  }
+
+  function addToMemo(budget, positionsLeft, value) {
+    if (!memo[positionsLeft]) {
+      memo[positionsLeft] = {};
+    }
+    memo[positionsLeft][budget] = value;
+  }
+
+  return mV(positions, budget);
 };
